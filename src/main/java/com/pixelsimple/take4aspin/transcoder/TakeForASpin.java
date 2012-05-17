8d5d3fc18158bc812d65ee75b71f7610015affd8
Akshay Sharma <cto@pixelsimple.com>
2012-05-17 13:55:14 +0530

    Adapted to app-core api changes

--- src/main/java/com/pixelsimple/take4aspin/transcoder/TakeForASpin.java
+++ src/main/java/com/pixelsimple/take4aspin/transcoder/TakeForASpin.java
@@ -9,14 +9,15 @@ import java.util.Map;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.pixelsimple.appcore.Registrable;
-import com.pixelsimple.appcore.RegistryService;
+import com.pixelsimple.appcore.registry.GenericRegistryEntry;
+import com.pixelsimple.appcore.registry.RegistryService;
 import com.pixelsimple.commons.media.Container;
 import com.pixelsimple.commons.media.MediaInspector;
 import com.pixelsimple.commons.media.exception.MediaException;
 import com.pixelsimple.take4aspin.init.Initializer;
-import com.pixelsimple.transcoder.TranscoderOutputSpec;
 import com.pixelsimple.transcoder.Transcoder;
+import com.pixelsimple.transcoder.TranscoderOutputSpec;
+import com.pixelsimple.transcoder.config.TranscoderRegistryKeys;
 import com.pixelsimple.transcoder.exception.TranscoderException;
 import com.pixelsimple.transcoder.profile.Profile;
 
@@ -86,8 +87,9 @@ public class TakeForASpin {
 		} 
 		
 		// TODO: In non-test code, this target profile will use a good profile match algorithm
+		GenericRegistryEntry entry =  RegistryService.getGenericRegistryEntry();
 		@SuppressWarnings("unchecked")
-		Map<String, Profile> profiles = (Map<String, Profile>) RegistryService.getRegisteredEntry(Registrable.MEDIA_PROFILES);
+		Map<String, Profile> profiles = (Map<String, Profile>) entry.getEntry(TranscoderRegistryKeys.MEDIA_PROFILES);
 		
 		Profile profile = profiles.get(profileToUse);
 		
